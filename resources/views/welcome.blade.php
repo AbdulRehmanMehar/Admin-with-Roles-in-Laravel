e3df886599b107dc05059a2e806c9b4ba19ea4de
Abdul Rehman <mehars.6925@gmail.com>
2019-04-10 18:59:09 +0500

    + homepage view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,22 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
+    <div class="container mt-4">
+        <h1>What does this application do?</h1>
+        <p>This application is simple eCommerce application. Anyone can register or login and place an order. Then the order is processed in three setps:</p>
+        <ul>
+            <li>When customer places the Order, it is sent to <b>Order Manager</b> which processes it.</li>
+            <li>Then Order is transfered to <b>Shipping Manager</b> which makes sure that the order is ready to be shipped.</li>
+            <li>When the order is processed by Shipping Manager, it is transfered to <b>Delivery Manager</b> which delivers the order.</li>
+        </ul>
+        <h2>Constraints</h2>
+        <p>Here are some constraints:</p>
+        <ul>
+            <li>We've 3 users of same role &amp; One user can handle 3 orders at a time.</li>
+            <li>If no user is free and i.e an order is placed, it is set to be pending.</li>
+            <li>When a user of that role (for which the order is pending) gets free, the order is assigned to that user.</li>
+        </ul>
+        <p>This application is built with laravel and mongodb but It doesn't use the Queue System of Laravel.</p>
+    </div>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+@endsection
